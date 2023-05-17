c319ad42b03f2b6ac6fdff4ef178b6ddb4e0a6cd
mrmckney <mrmckney@gmail.com>
2023-05-16 22:19:05 -0400

    deploy new route

--- src/services/fetch-api.ts
+++ src/services/fetch-api.ts
@@ -1,7 +1,7 @@
 import { WeatherData, ForecastData, Coords } from "./interfaces"
 
 export const fetchWeatherData = async (coords: Coords) => {
-    const response: Response = await fetch(`http://localhost:5001/api/weather`, {
+    const response: Response = await fetch(`https://weather-app-api-orcin.vercel.app/api/weather`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -13,7 +13,7 @@ export const fetchWeatherData = async (coords: Coords) => {
 }
 
 export const fetchForecastData = async (coords: Coords) => {
-    const response: Response = await fetch(`http://localhost:5001/api/forecast`, {
+    const response: Response = await fetch(`https://weather-app-api-orcin.vercel.app/api/forecast`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -25,7 +25,7 @@ export const fetchForecastData = async (coords: Coords) => {
 }
 
 export const fetchSearchData = async (word: string) => {
-    const response: Response = await fetch(`http://localhost:5001/api/search`, {
+    const response: Response = await fetch(`https://weather-app-api-orcin.vercel.app/api/search`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
